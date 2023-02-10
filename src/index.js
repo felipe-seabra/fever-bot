a644b135f1f29b872856e1f4bbdf365d2898050b
Felipe Seabra <feliperoce@gmail.com>
2023-02-10 19:49:37 -0300

    feat: index.js

---
+++ src/index.js
@@ -0,0 +1,89 @@
+/* eslint-disable sonarjs/cognitive-complexity */
+/* eslint-disable max-len */
+/* eslint-disable complexity */
+/* eslint-disable max-lines-per-function */
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable no-restricted-syntax */
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+
+const dotenv = require('dotenv');
+
+dotenv.config();
+const { TOKEN } = process.env;
+
+const fs = require('node:fs');
+const path = require('node:path');
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(`Esse comando em ${filePath} está com "data" ou "execute ausentes"`);
+  }
+}
+
+// Login do bot
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Login performed as ${c.user.tag}`);
+});
+client.login(TOKEN);
+
+client.on('ready', () => {
+  console.log(`${client.user.tag} are you listening`);
+
+  const data = [
+    'Programming...',
+    'type /help for more',
+  ];
+  const cont = 0;
+  setInterval(() => client.user.setPresence({
+    activities: [{
+      name: `${data[(cont + 1) % data.length]}`,
+      type: 0,
+    }],
+  }), 1000 * 30);
+  client.user.setStatus('dnd');
+
+  console.log(`${client.user.tag} status: ${client.user.presence.status}`);
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (interaction.isStringSelectMenu()) {
+    const selected = interaction.values[0];
+    if (selected === 'javascript') {
+      await interaction
+        .reply('Documentação do Javascript: https://developer.mozilla.org/en-US/docs/Web/JavaScript');
+    } else if (selected === 'python') {
+      await interaction
+        .reply('Documentação do Python: https://www.python.org');
+    } else if (selected === 'csharp') {
+      await interaction
+        .reply('Documentação do C#: https://learn.microsoft.com/en-us/dotnet/csharp/');
+    } else if (selected === 'discordjs') {
+      await interaction
+        .reply('Documentação do Discord.js: https://discordjs.guide/#before-you-begin');
+    }
+  }
+  if (!interaction.isChatInputCommand()) return;
+  const command = interaction.client.commands.get(interaction.commandName);
+  if (!command) {
+    console.error('Command not found');
+    return;
+  }
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply('Houve um erro ao executar esse comando!');
+  }
+});
