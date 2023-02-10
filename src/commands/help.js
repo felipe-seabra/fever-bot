cd2321cbefe31aab3dac9ab5856c604bfe06bd01
Felipe Seabra <feliperoce@gmail.com>
2023-02-10 19:45:25 -0300

    feat: help.js

---
+++ src/commands/help.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+const botCommands = new EmbedBuilder()
+  .setColor('Orange')
+  .setTitle('Comandos do Bot')
+  .addFields(
+    { name: '\u200B', value: '\u200B' },
+    { name: '/ping', value: 'Responde com "Pong!"', inline: true },
+    { name: '/git', value: 'Lista os comandos mais utilizados no GitHub', inline: true },
+    { name: '/docs', value: 'Um seletor de linguages de programação', inline: true },
+    { name: '\u200B', value: '\u200B' },
+    { name: '/help', value: 'Lista os comandos do Bot', inline: true },
+  );
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('help')
+    .setDescription('Listar comandos do Bot'),
+
+  async execute(interaction) {
+    await interaction.reply({ embeds: [botCommands] });
+  },
+};
