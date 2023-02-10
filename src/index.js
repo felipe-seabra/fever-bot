56fa07ac0befb0baf21b3dca3454da3249961902
Felipe Seabra <feliperoce@gmail.com>
2023-02-10 20:03:29 -0300

    build: deploy commands

--- src/index.js
+++ src/index.js
@@ -1,89 +1,80 @@
-/* eslint-disable sonarjs/cognitive-complexity */
-/* eslint-disable max-len */
-/* eslint-disable complexity */
-/* eslint-disable max-lines-per-function */
+/* eslint-disable no-return-assign */
 /* eslint-disable global-require */
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable no-restricted-syntax */
-const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
-
+const { Client, Events, GatewayIntentBits } = require('discord.js');
 const dotenv = require('dotenv');
+const fs = require('fs');
+const path = require('path');
 
 dotenv.config();
 const { TOKEN } = process.env;
 
-const fs = require('node:fs');
-const path = require('node:path');
-
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
-
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Map();
 
-client.commands = new Collection();
+const loadCommands = () => {
+  const commandsPath = path.join(__dirname, 'commands');
+  const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  if ('data' in command && 'execute' in command) {
-    client.commands.set(command.data.name, command);
-  } else {
-    console.log(`Esse comando em ${filePath} está com "data" ou "execute ausentes"`);
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    if (command.data && command.execute) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(`${filePath} is missing 'data' or 'execute' property.`);
+    }
   }
-}
+};
 
-// Login do bot
-client.once(Events.ClientReady, (c) => {
-  console.log(`Ready! Login performed as ${c.user.tag}`);
-});
-client.login(TOKEN);
-
-client.on('ready', () => {
-  console.log(`${client.user.tag} are you listening`);
+const setBotStatus = () => {
+  client.user.setStatus('dnd');
 
   const data = [
     'Programming...',
     'type /help for more',
   ];
-  const cont = 0;
-  setInterval(() => client.user.setPresence({
-    activities: [{
-      name: `${data[(cont + 1) % data.length]}`,
-      type: 0,
-    }],
-  }), 1000 * 30);
-  client.user.setStatus('dnd');
 
-  console.log(`${client.user.tag} status: ${client.user.presence.status}`);
+  let count = 0;
+
+  setInterval(() =>
+    client.user.setPresence({
+      activities: [{
+        name: `${data[(count += 1) % data.length]}`,
+        type: 0,
+      }],
+    }), 30000);
+};
+
+client.once(Events.ClientReady, () => {
+  console.log(`Bot logged in as ${client.user.tag}.`);
+});
+
+client.on('ready', () => {
+  console.log(`${client.user.tag} is ready.`);
+  loadCommands();
+  setBotStatus();
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
-  if (interaction.isStringSelectMenu()) {
-    const selected = interaction.values[0];
-    if (selected === 'javascript') {
-      await interaction
-        .reply('Documentação do Javascript: https://developer.mozilla.org/en-US/docs/Web/JavaScript');
-    } else if (selected === 'python') {
-      await interaction
-        .reply('Documentação do Python: https://www.python.org');
-    } else if (selected === 'csharp') {
-      await interaction
-        .reply('Documentação do C#: https://learn.microsoft.com/en-us/dotnet/csharp/');
-    } else if (selected === 'discordjs') {
-      await interaction
-        .reply('Documentação do Discord.js: https://discordjs.guide/#before-you-begin');
-    }
-  }
-  if (!interaction.isChatInputCommand()) return;
-  const command = interaction.client.commands.get(interaction.commandName);
-  if (!command) {
-    console.error('Command not found');
+  if (!interaction.isChatInputCommand()) {
     return;
   }
+
+  const command = client.commands.get(interaction.commandName);
+  if (!command) {
+    console.error(`Command '${interaction.commandName}' not found.`);
+    return;
+  }
+
   try {
     await command.execute(interaction);
   } catch (error) {
     console.error(error);
-    await interaction.reply('Houve um erro ao executar esse comando!');
+    await interaction.reply('Error executing command.');
   }
 });
+
+client.login(TOKEN);
